256904e2d5cc43d3f91c2c86c6e44d430913a7b2
CarlosGalan194 <a01425194@tec.mx>
2026-09-17 18:34:08 -0600

    velocidad de fantasmos aumentada

--- pacman.py
+++ pacman.py
@@ -1,17 +1,5 @@
-"""Pacman, classic arcade game.
-
-Exercises
-
-1. Change the board.
-2. Change the number of ghosts.
-3. Change where pacman starts.
-4. Make the ghosts faster/slower.
-5. Make the ghosts smarter.
-"""
-
 from random import choice
 from turtle import *
-
 from freegames import floor, vector
 
 state = {'score': 0}
@@ -20,12 +8,11 @@ writer = Turtle(visible=False)
 aim = vector(5, 0)
 pacman = vector(-40, -80)
 ghosts = [
-    [vector(-180, 160), vector(5, 0)],
-    [vector(-180, -160), vector(0, 5)],
-    [vector(100, 160), vector(0, -5)],
-    [vector(100, -160), vector(-5, 0)],
+    [vector(-180, 160), vector(10, 0)],
+    [vector(-180, -160), vector(0, 10)],
+    [vector(100, 160), vector(0, -10)],
+    [vector(100, -160), vector(-10, 0)],
 ]
-# fmt: off
 tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
@@ -48,11 +35,9 @@ tiles = [
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 ]
-# fmt: on
-
 
 def square(x, y):
-    """Draw square using path at (x, y)."""
+    "Draw square using path at (x, y)."
     path.up()
     path.goto(x, y)
     path.down()
@@ -64,17 +49,15 @@ def square(x, y):
 
     path.end_fill()
 
-
 def offset(point):
-    """Return offset of point in tiles."""
+    "Return offset of point in tiles."
     x = (floor(point.x, 20) + 200) / 20
     y = (180 - floor(point.y, 20)) / 20
     index = int(x + y * 20)
     return index
 
-
 def valid(point):
-    """Return True if point is valid in tiles."""
+    "Return True if point is valid in tiles."
     index = offset(point)
 
     if tiles[index] == 0:
@@ -87,9 +70,8 @@ def valid(point):
 
     return point.x % 20 == 0 or point.y % 20 == 0
 
-
 def world():
-    """Draw world using path."""
+    "Draw world using path."
     bgcolor('black')
     path.color('blue')
 
@@ -106,9 +88,8 @@ def world():
                 path.goto(x + 10, y + 10)
                 path.dot(2, 'white')
 
-
 def move():
-    """Move pacman and all ghosts."""
+    "Move pacman and all ghosts."
     writer.undo()
     writer.write(state['score'])
 
@@ -135,10 +116,10 @@ def move():
             point.move(course)
         else:
             options = [
-                vector(5, 0),
-                vector(-5, 0),
-                vector(0, 5),
-                vector(0, -5),
+                vector(10, 0),
+                vector(-10, 0),
+                vector(0, 10),
+                vector(0, -10),
             ]
             plan = choice(options)
             course.x = plan.x
@@ -156,13 +137,11 @@ def move():
 
     ontimer(move, 100)
 
-
 def change(x, y):
-    """Change pacman aim if valid."""
+    "Change pacman aim if valid."
     if valid(pacman + vector(x, y)):
         aim.x = x
         aim.y = y
-
 
 setup(420, 420, 370, 0)
 hideturtle()
